0892b7bd48db0204275202d958523991d9dd571b
tukait01 <tunahan.kaya@web.de>
2021-04-14 13:53:54 +0200

    added again my-vue-project

---
+++ my-vue-project/src/components/UserList.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="user in users"
+            :key="user.name"
+            @click="user.showAge = true"
+            :class="colorize(user.age)"
+        >
+                {{ user.name }}
+                <span v-if="user.showAge">{{ user.age }}</span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    users: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    colorize(age) {
+      return {
+        red: age < 50,
+        blue: age > 50,
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+div ul {
+    margin-top: 150px;
+}
+
+.red {
+    color: red;
+}
+
+.blue {
+    color: blue;
+}
+</style>
